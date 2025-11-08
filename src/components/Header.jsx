bf0b01e0a18aa2520433adfdaaae88f9382eab23
Guilherme Monni <gmonni20@gmail.com>
2025-11-08 20:54:07 -0300

    add fonts and logo and fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
 function Header(){
 
     return(
-        <div className="bg-primary">teste aaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+        <>
+            <div className="bg-deep h-40 flex items-center justify-center flex-col p-3">
+                <img src="/images/logo-app.png" alt="Imagem logo app." className="rounded-full max-h-24 max-w-32" />
+                <nav>
+                    <ul className="inline-flex mt-5 gap-2 text-gray-100 *:cursor-pointer *:hover:text-primary *:duration-150 *:font-rubik">
+                        <li>Início</li>
+                        <li>Serviços</li>
+                        <li>Redes</li>
+                        <li>Sobre</li>
+                    </ul>
+                </nav>
+            </div>
+        </>
     )
 }
 
