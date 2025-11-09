5010736ea9ea3ac24879f0be3df185aef446582e
Guilherme Monni <gmonni20@gmail.com>
2025-11-08 21:16:27 -0300

    init container content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header"
+import Container from "./components/Container"
 
 function App(){
   
   return(
-    <Header />
+    <>
+      <Header />
+      <Container />
+    </>
   )
 }
 
---
+++ src/components/Container.jsx
@@ -0,0 +1,11 @@
+function Container(){
+
+    return(
+        <main className="bg-alternate w-full h-full">
+            <h3>Boas vindas ao KindlyHelp!</h3>
+            <p>Neste aplicativo temos o objetivo de divulgar serviços sociais gratuitos. Fique a vontade para se informar sobre. Caso se sinta à votade para anunciar um serviço que não está na plataforma, fique à vontade para enviar informações para nossa equipe analisar.</p>
+        </main>
+    )
+}
+
+export default Container
